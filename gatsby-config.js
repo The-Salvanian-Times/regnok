bb37cefc50d55a2ac2d9b9024168af998c7d35b5
Immanuel Salva <83900884+ImmanuelSalva@users.noreply.github.com>
2021-11-01 14:48:47 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Regnok.com",
+    description: "Regnok.com News Website",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
